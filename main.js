f38f3380069b62184f6492bf65fe3739b4a0d252
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-27 22:58:25 +0530

    added copy to clip

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 function submit() {
 	document.location('alert.html');
 }   
+
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+} 
